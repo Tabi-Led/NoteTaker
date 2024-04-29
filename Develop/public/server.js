56965e5e6b9c18fa23167c2ca8e6c8a5f10a31f7
tabi-led <tabiletallecx@gmail.com>
2024-04-29 09:27:22 -0500

    fixed json and note html

---
+++ Develop/public/server.js
@@ -0,0 +1,25 @@
+//TODO Make project
+const express = require('express');
+const path = require('path');
+const api = require('./routes');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
+app.use('/api', api);
+
+app.get('/', (req, res) =>
+res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
+app.listen(PORT, () =>
+console.log(`app listenting `)
+);
